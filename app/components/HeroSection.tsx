df0f25a08594bb286497a91f00bca03102391ef7
Richard Pachuliia <gorgerichig@gmail.com>
2025-12-10 15:21:34 +0300

    image

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,7 +1,6 @@
 import { TopButtons } from './TopButtons';
 
-const heroImage =
-  'https://images.unsplash.com/photo-1615403294586-8c6df4cab0b8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxwZWFjZWZ1bCUyMGNlbWV0ZXJ5JTIwdHJlZXN8ZW58MXx8fHwxNzYzNzYwMjQ1fDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral';
+const heroImage = "url('/hero-forest.jpg')"
 
 export function HeroSection() {
   return (
