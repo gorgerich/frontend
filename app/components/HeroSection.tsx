ea8214188a4c3f57783a64d08706e1ad4d2f403b
Richard Pachuliia <gorgerichig@gmail.com>
2025-12-11 22:52:13 +0300

    i

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -56,10 +56,10 @@ export function HeroSection() {
                   fontWeight: 400,
                 }}
               >
-                <span className="mb-1 block text-3xl lg:text-10xl xl:text-15xl">
+                <span className="mb-1 block text-3xl lg:text-1xl xl:text-3xl">
                   Цифровой помощник по самостоятельной
                 </span>
-                <span className="block text-3xl lg:text-10xl xl:text-15xl">
+                <span className="block text-3xl lg:text-1xl xl:text-3xl">
                   организации прощания без агентств и давления
                 </span>
               </span>
